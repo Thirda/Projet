ffc59cf87557dc83001eaa6f8bc85c961220a7ee
Emeline <leduc.emeline44@gmail.com>
2021-03-19 11:38:15 +0100

    problem burger-menu with display flex in nav

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+// Burger menu
+const menuHam = document.querySelector(".menu-ham");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
 
+menuHam.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+  links.forEach((link) => {
+    link.classList.toggle("fade");
+  });
+});
