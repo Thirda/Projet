effab54d7702db82bc744b80baba3769ef0b5704
Emeline <leduc.emeline44@gmail.com>
2021-03-23 10:39:41 +0100

    add sticky navbar

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Burger menu
-const nav = document.querySelector("nav");
+const navbar = document.querySelector("nav");
 const menuHam = document.querySelector(".menu-ham");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 
 menuHam.addEventListener("click", () => {
-  nav.classList.toggle("visible");
+  navbar.classList.toggle("visible");
   menuHam.classList.toggle("active");
   navLinks.classList.toggle("open");
   links.forEach((link) => {
@@ -13,7 +13,24 @@ menuHam.addEventListener("click", () => {
   });
 });
 
+
 // Disable scrolling in the background when burger menu is open
 function lockScroll() {
   document.body.classList.toggle("lock-scroll");
+}
+
+
+//Sticky navbar
+window.onscroll = function () {
+  myFunction();
+};
+
+const sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
